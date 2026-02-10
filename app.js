3bdf115109bf26c585d37a94d5490adcea0993a8
sumitdoescode <sumit.does.code@gmail.com>
2026-02-10 21:19:45 +0530

    changed default port

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.listen(80, () => {
-    console.log("Server is running on port 80");
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
 });
