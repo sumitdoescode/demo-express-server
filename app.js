b7132a724cfdd7a262984fd37286f7c1df6b2ffb
sumitdoescode <sumit.does.code@gmail.com>
2026-02-10 23:21:54 +0530

    changed port

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.listen(443, () => {
-    console.log("Server is running on port 443");
+app.listen(80, () => {
+    console.log("Server is running on port 80");
 });
