47f5439c955ac6573162dac1c7a1a641c0f8372a
sumitdoescode <sumit.does.code@gmail.com>
2026-02-11 00:41:31 +0530

    hey there

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
