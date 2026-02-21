6ab58f94976d22f6d98d37150269db7d1d90775e
sumitdoescode <sumit.does.code@gmail.com>
2026-02-21 11:43:33 +0530

    text changes

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express";
 
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("This is a clothes application");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
